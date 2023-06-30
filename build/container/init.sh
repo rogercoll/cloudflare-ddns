9d6e6f1e09c4011d5193c38874290d69cca887c2
Roger Coll <rogercoll@protonmail.com>
2023-06-30 13:50:02 +0200

    feat: add IP_CHECKER env

--- build/container/init.sh
+++ build/container/init.sh
@@ -5,8 +5,14 @@ if [[ -z "${TOKEN}" && -z "${RECORD_ID}" && -z "${ZONE_ID}" ]]; then
         exit 1
 fi
 
-if [[ -z "${LONG_RUNNING}" ]]; then
-        exec /target/debug/cloudflare-ddns --token ${TOKEN} --record-name ${RECORD_NAME} --zone-id ${ZONE_ID}
-  else
-        exec /target/debug/cloudflare-ddns --token ${TOKEN} --record-name ${RECORD_NAME} --zone-id ${ZONE_ID} --long-running ${LONG_RUNNING}
+GLOBAL_ARGS=""
+
+if [[ "${LONG_RUNNING}" ]]; then
+    GLOBAL_ARGS="$GLOBAL_ARGS --long-running $LONG_RUNNING"
 fi
+
+if [[ "${IP_CHECKER}" ]]; then
+    GLOBAL_ARGS="$GLOBAL_ARGS --ip_checker $IP_CHECKER"
+fi
+
+exec /target/debug/cloudflare-ddns --token ${TOKEN} --record-name ${RECORD_NAME} --zone-id ${ZONE_ID} ${GLOBAL_ARGS}
